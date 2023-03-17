cbe4900502c5134b395f0e3dc8e691851909bcad
horizon <axmm.123@foxmail.com>
2023-03-17 20:05:16 +0800

    配置 导入tabbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+/* const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+} */
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+      path:'',
+      redirect:'/home'
+    },
+    {
+      path:'/home',
+      component:()=>import('views/home/Home')
+    },
+    {
+      path:'/category',
+      component:()=>import('views/category/Category')
+    },
+    {
+      path:'/cart',
+      component:()=>import('views/cart/Cart')
+    },
+    {
+      path:'/profile',
+      component:()=>import('views/profile/Profile')
+    }
 ]
 
 const router = new VueRouter({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,//自带的
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'views': '@/views',
+        'components': '@/components',
+        'network': '@/network',
+        'common': '@/common',
+        'assets': '@/assets',
+      }
+    }
+  }
 }
