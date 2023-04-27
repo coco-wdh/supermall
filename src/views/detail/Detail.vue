bf13b5b2992adac61c46fd75d8cd80fc857d2d14
horizon <axmm.123@foxmail.com>
2023-04-27 19:23:34 +0800

    详情页商品和详情信息展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,26 +1,43 @@
 <template>
-  <div>
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :images="topImages"></detail-swiper>
+  <div id="detail">
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :images="topImages"></detail-swiper>
+      <detail-goods-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-details-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+    </scroll>
   </div>
 </template>
 
 <script>
-import { getDetail } from 'network/detail';
+import { getDetail, Goods, Shop, GoodsParam } from '@/network/detail'
+import Scroll from '@/components/common/scroll/Scroll.vue'
 
-import DetailSwiper from './childComps/DetailSwiper'
-import DetailNavBar from './childComps/DetailNavBar'
+import DetailNavBar from './childComps/DetailNavBar.vue'
+import DetailSwiper from './childComps/DetailSwiper.vue'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo.vue'
+import DetailShopInfo from './childComps/DetailShopInfo.vue'
+import DetailDetailsInfo from './childComps/DetailDetailsInfo.vue'
 
 export default {
   name: "Detail",
-  components:{
+  components: {
+    DetailNavBar,
+    Scroll,
     DetailSwiper,
-    DetailNavBar
+    DetailGoodsInfo,
+    DetailShopInfo,
+    DetailDetailsInfo
   },
   data() {
     return {
       id: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {}
     }
   },
   created() {
@@ -28,9 +45,37 @@ export default {
     getDetail(this.id).then(res => {
       const data = res.result;
       this.topImages = data.itemInfo.topImages;
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
+      this.shop = new Shop(data.shopInfo);
+      this.detailInfo = data.detailInfo;
+      console.log(this.detailInfo);
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
     });
   },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+
+.content {
+  overflow: hidden;
+  height: calc(100% - 44px);
+}
+</style>
